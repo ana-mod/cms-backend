142d5e1ed6286c01e6a1c7a5fb16b6076cc15a5f
Anke <ania.helena.mod@gmail.com>
2021-05-30 22:03:34 +0200

    conference entity updated

--- src/main/java/com/example/cms/models/Conference.java
+++ src/main/java/com/example/cms/models/Conference.java
@@ -1,9 +1,16 @@
 package com.example.cms.models;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public
@@ -16,23 +23,112 @@ class Conference {
 	private String topic;
 	
 	@Column
-	private String author;
+	private String name;
 
-	public Conference(String topic, String author) {
+	@Column
+	private Date startDate;
+	
+	@Column
+	private Date endDate;
+
+	@Column(length = 1000)
+	private String description;
+
+	@OneToMany(mappedBy = "conference")
+	private List<Notification> notifications;
+
+	@JoinTable
+	@ManyToMany
+	 private Set<User> users;
+
+	@OneToMany(mappedBy = "conference")
+	private List<Presentation> presentations;
+
+	public Conference(String topic, String name, Date startDate, Date endDate, String description,
+			List<Notification> notifications, Set<User> users, List<Presentation> presentations) {
 		this.topic = topic;
-		this.author = author;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.description = description;
+		this.notifications = notifications;
+		this.users = users;
+		this.presentations = presentations;
 	}
 
 	public Long getId() {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getTopic() {
 		return topic;
 	}
 
-	public String getAuthor() {
-		return author;
+	public void setTopic(String topic) {
+		this.topic = topic;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Notification> getNotifications() {
+		return notifications;
+	}
+
+	public void setNotifications(List<Notification> notifications) {
+		this.notifications = notifications;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+	public List<Presentation> getPresentations() {
+		return presentations;
+	}
+
+	public void setPresentations(List<Presentation> presentations) {
+		this.presentations = presentations;
+	}
+
+
+
 
 }
