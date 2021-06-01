e554cb4c0a617ceddf0cbc96ac75e37bc5c37bcc
wiktor <wkilijan@gmail.com>
2021-06-01 19:15:29 +0200

    creating conference with author

--- src/main/java/com/example/cms/controllers/ConferenceController.java
+++ src/main/java/com/example/cms/controllers/ConferenceController.java
@@ -1,33 +1,53 @@
 package com.example.cms.controllers;
 
+import com.example.cms.models.Author;
 import com.example.cms.models.Conference;
+import com.example.cms.models.User;
+import com.example.cms.repositories.AuthorRepository;
 import com.example.cms.repositories.ConferenceRepository;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class ConferenceController {
 
-	private final ConferenceRepository conferenceRepository;
+    private final ConferenceRepository conferenceRepository;
+    private final AuthorRepository authorsRepository;
 
-	@GetMapping("/")
-	String hello() {
-		return "Hello there";
-	}
+    @GetMapping("/")
+    String hello() {
+        return "Hello there";
+    }
 
-	@GetMapping("/conferences")
-	Iterable<Conference> conferences() {
-		return conferenceRepository.findAll();
-	}
+    @GetMapping("/conferences")
+    Iterable<Conference> conferences() {
+        return conferenceRepository.findAll();
+    }
 
-	@PostMapping("/conference")
-	ResponseEntity<?> createConference(@RequestBody Conference conferenceToAdd) {
-		Conference result = conferenceRepository.save(conferenceToAdd);
-		return ResponseEntity.ok().build();
-	}
+    @PostMapping("/conference")
+    ResponseEntity<Conference> createConference(@RequestBody Conference conferenceToAdd) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        Author author;
+        if (authorsRepository.existsByUserId(user.getId())) {
+            author = authorsRepository.findByUserId(user.getId());
+        } else {
+            author = new Author(user);
+            authorsRepository.save(author);
+        }
+        conferenceToAdd.setAuthor(author);
 
-	ConferenceController(ConferenceRepository conferenceRepository) {
-		this.conferenceRepository = conferenceRepository;
-	}
+        Conference result = conferenceRepository.save(conferenceToAdd);
+        return ResponseEntity.ok(result);
+    }
+
+    ConferenceController(ConferenceRepository conferenceRepository, AuthorRepository authorsRepository) {
+        this.conferenceRepository = conferenceRepository;
+        this.authorsRepository = authorsRepository;
+    }
 }
---
+++ src/main/java/com/example/cms/repositories/AuthorRepository.java
@@ -0,0 +1,14 @@
+package com.example.cms.repositories;
+
+import com.example.cms.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    public boolean existsByUserId(Long userId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM cms_author JOIN cms_user WHERE UserId=:userId LIMIT 1")
+    public Author findByUserId(@Param("userId") Long userId);
+}
