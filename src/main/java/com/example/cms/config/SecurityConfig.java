24544c8866c16cde2d5a5bd4054b6f92859d030a
bigfoxxx <ph.cool97@gmail.com>
2021-06-01 13:18:19 +0200

    Fix to security config

--- src/main/java/com/example/cms/config/SecurityConfig.java
+++ src/main/java/com/example/cms/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/api/auth").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(entryPoint)
