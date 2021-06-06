f54546c668a6ad0af310770c4089c76cafb0da70
Piotr Kedra <piotr.k9750@gmail.com>
2021-06-06 13:05:34 +0200

    removed not building code
    @andresnortesv

--- src/main/java/com/example/cms/repositories/ConferenceRepository.java
+++ src/main/java/com/example/cms/repositories/ConferenceRepository.java
@@ -2,13 +2,6 @@ package com.example.cms.repositories;
 
 import com.example.cms.models.Conference;
 
-import antlr.collections.List;
-
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ConferenceRepository extends CrudRepository<Conference, Long> {
-	@Query("select s from Conference where topic like %?1%")
-	List<Conference> findByName(String search, Pageable pageable);
-}
+public interface ConferenceRepository extends CrudRepository<Conference, Long> {}
