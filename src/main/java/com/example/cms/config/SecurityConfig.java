c14b98b0d6f42c534da2ccc09c67e0cfa47406d7
Piotr Kedra <piotr.k9750@gmail.com>
2021-06-06 13:09:56 +0200

    /conferences not working

--- src/main/java/com/example/cms/config/SecurityConfig.java
+++ src/main/java/com/example/cms/config/SecurityConfig.java
@@ -44,9 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .antMatchers("/conferences").permitAll()
-                .anyRequest().authenticated()
-                .and()
                 .exceptionHandling().authenticationEntryPoint(entryPoint)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
