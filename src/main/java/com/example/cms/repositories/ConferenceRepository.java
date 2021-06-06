5c74595d164a65e3bdac2368ebd3e59b31403b1d
andre <andre@DESKTOP-U4PN7IO>
2021-06-06 09:31:55 +0200

    SearchByFilter

--- src/main/java/com/example/cms/repositories/ConferenceRepository.java
+++ src/main/java/com/example/cms/repositories/ConferenceRepository.java
@@ -2,6 +2,13 @@ package com.example.cms.repositories;
 
 import com.example.cms.models.Conference;
 
+import antlr.collections.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ConferenceRepository extends CrudRepository<Conference, Long> {}
+public interface ConferenceRepository extends CrudRepository<Conference, Long> {
+	@Query("select s from Conference where topic like %?1%")
+	List<Conference> findByName(String search, Pageable pageable);
+}
